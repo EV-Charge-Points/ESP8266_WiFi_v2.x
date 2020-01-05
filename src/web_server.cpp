EV-Charge-Points/ESP32_WiFi_V3.x

Added API to store the cost of a kWh

src/web_server.cpp
@@ -436,6 +436,27 @@ handleSaveOhmkey(MongooseHttpServerRequest *request) {
 }
 
 // -------------------------------------------------------------------
+// Save the Ohm keyto EEPROM
+// url: /handleSaveUnitCost
+// -------------------------------------------------------------------
+void
+handleSaveUnitCost(MongooseHttpServerRequest *request) {
+  MongooseHttpServerResponseStream *response;
+  if(false == requestPreProcess(request, response, CONTENT_TYPE_TEXT)) {
+    return;
+  }
+
+  String format = request->getParam("format");
+  String cost = request->getParam("cost");
+
+  config_save_unit_cost(format, (uint32_t)(cost.toDouble() * 1000));
+
+  response->setCode(200);
+  response->print("saved");
+  request->send(response);
+}
+
+// -------------------------------------------------------------------
 // Returns status json
 // url: /status
 // -------------------------------------------------------------------
@@ -595,7 +616,9 @@ handleConfig(MongooseHttpServerRequest *request) {
   }
   s += "\",";
   s += "\"hostname\":\"" + esp_hostname + "\",";
-  s += "\"ohm_enabled\":" + String(config_ohm_enabled() ? "true" : "false");
+  s += "\"ohm_enabled\":" + String(config_ohm_enabled() ? "true" : "false")+",";
+  s += "\"unit_cost_value\":"+String((double)unit_cost / 1000.0)+",";
+  s += "\"unit_cost_format\":\"" + unit_cost_format + "\"";
   s += "}";
 
   response->setCode(200);
@@ -1019,6 +1042,7 @@ web_server_setup() {
   server.on("/saveadmin$", handleSaveAdmin);
   server.on("/saveadvanced$", handleSaveAdvanced);
   server.on("/saveohmkey$", handleSaveOhmkey);
+  server.on("/saveunitcost$", handleSaveUnitCost);
   server.on("/reset$", handleRst);
   server.on("/restart$", handleRestart);
   server.on("/rapi$", handleRapi);

EV-Charge-Points/ESP8266_WiFi_v2.x

src/web_server.cpp
@@ -433,6 +433,27 @@ handleSaveOhmkey(AsyncWebServerRequest *request) {
 }
 
 // -------------------------------------------------------------------
+// Save the Ohm keyto EEPROM
+// url: /handleSaveUnitCost
+// -------------------------------------------------------------------
+void
+handleSaveUnitCost(AsyncWebServerRequest *request) {
+  AsyncResponseStream *response;
+  if(false == requestPreProcess(request, response, CONTENT_TYPE_TEXT)) {
+    return;
+  }
+
+  String format = request->arg("format");
+  String cost = request->arg("cost");
+
+  config_save_unit_cost(format, (uint32_t)(cost.toFloat() * 1000));
+
+  response->setCode(200);
+  response->print("saved");
+  request->send(response);
+}
+
+// -------------------------------------------------------------------
 // Returns status json
 // url: /status
 // -------------------------------------------------------------------
@@ -589,7 +610,9 @@ handleConfig(AsyncWebServerRequest *request) {
   }
   s += "\",";
   s += "\"hostname\":\"" + esp_hostname + "\",";
-  s += "\"ohm_enabled\":" + String(config_ohm_enabled() ? "true" : "false");
+  s += "\"ohm_enabled\":" + String(config_ohm_enabled() ? "true" : "false")+",";
+  s += "\"unit_cost_value\":"+String((double)unit_cost / 1000.0)+",";
+  s += "\"unit_cost_format\":\"" + unit_cost_format + "\"";
   s += "}";
 
   response->setCode(200);
@@ -959,6 +982,7 @@ web_server_setup() {
   server.on("/saveadmin", handleSaveAdmin);
   server.on("/saveadvanced", handleSaveAdvanced);
   server.on("/saveohmkey", handleSaveOhmkey);
+  server.on("/saveunitcost", handleSaveUnitCost);
   server.on("/reset", handleRst);
   server.on("/restart", handleRestart);
   server.on("/rapi", handleRapi);