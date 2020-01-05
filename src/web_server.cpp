EV-Charge-Points/ESP32_WiFi_V3.x

Changed how unit cost format is stored to make it more portable

src/web_server.cpp
@@ -446,10 +446,11 @@ handleSaveUnitCost(MongooseHttpServerRequest *request) {
     return;
   }
 
-  String format = request->getParam("format");
+  String local = request->getParam("local");
+  String code = request->getParam("code");
   String cost = request->getParam("cost");
 
-  config_save_unit_cost(format, (uint32_t)(cost.toDouble() * 1000));
+  config_save_unit_cost(local, code, (uint32_t)(cost.toDouble() * UNIT_COST_MULTIPLIER));
 
   response->setCode(200);
   response->print("saved");
@@ -617,8 +618,9 @@ handleConfig(MongooseHttpServerRequest *request) {
   s += "\",";
   s += "\"hostname\":\"" + esp_hostname + "\",";
   s += "\"ohm_enabled\":" + String(config_ohm_enabled() ? "true" : "false")+",";
-  s += "\"unit_cost_value\":"+String((double)unit_cost / 1000.0)+",";
-  s += "\"unit_cost_format\":\"" + unit_cost_format + "\"";
+  s += "\"unit_cost_value\":"+String((double)unit_cost / UNIT_COST_MULTIPLIER)+",";
+  s += "\"unit_cost_local\":\"" + unit_cost_local + "\",";
+  s += "\"unit_cost_code\":\"" + unit_cost_code + "\"";
   s += "}";
 
   response->setCode(200);

EV-Charge-Points/ESP8266_WiFi_v2.x

src/web_server.cpp
@@ -443,10 +443,11 @@ handleSaveUnitCost(AsyncWebServerRequest *request) {
     return;
   }
 
-  String format = request->arg("format");
+  String local = request->arg("local");
+  String code = request->arg("code");
   String cost = request->arg("cost");
 
-  config_save_unit_cost(format, (uint32_t)(cost.toFloat() * 1000));
+  config_save_unit_cost(local, code, (uint32_t)(cost.toFloat() * UNIT_COST_MULTIPLIER));
 
   response->setCode(200);
   response->print("saved");
@@ -611,8 +612,9 @@ handleConfig(AsyncWebServerRequest *request) {
   s += "\",";
   s += "\"hostname\":\"" + esp_hostname + "\",";
   s += "\"ohm_enabled\":" + String(config_ohm_enabled() ? "true" : "false")+",";
-  s += "\"unit_cost_value\":"+String((double)unit_cost / 1000.0)+",";
-  s += "\"unit_cost_format\":\"" + unit_cost_format + "\"";
+  s += "\"unit_cost_value\":"+String((double)unit_cost / UNIT_COST_MULTIPLIER)+",";
+  s += "\"unit_cost_local\":\"" + unit_cost_local + "\",";
+  s += "\"unit_cost_code\":\"" + unit_cost_code + "\"";
   s += "}";
 
   response->setCode(200);